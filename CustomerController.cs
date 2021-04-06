8ce5432461fc1ec644bc66317d62a9ea021ec92c
ArpanDasgupta <82029164+ArpanDasgupta@users.noreply.github.com>
2021-04-06 15:57:09 +0530

    Add files via upload

---
+++ CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BankingLib;
+using Banking.Models;
+namespace Banking.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var rm = (RegisterModel)Session["User"];
+           
+            ViewBag.User = rm.Account_Name;
+            return View(rm);
+        }
+    }
+}
